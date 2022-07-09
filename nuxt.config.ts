6ef4d0ff1c17e3267844e48fe4b1d53e82525f4c
Jarod <jarod@studiotreble.com>
2022-07-09 23:27:15 +0100

    build: set target static

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,10 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  target: 'static',
+
   buildModules: ['@nuxtjs/tailwindcss'],
+
   css: [
     '@/assets/global.scss',
   ],
